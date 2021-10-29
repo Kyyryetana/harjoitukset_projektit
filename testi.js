ecb4ee82ff190a17ae0d80de3cc43fcde3d1f699
nikoalander1986 <niko.alander@hotmail.com>
2021-10-30 00:02:21 +0300

    js harjoittelua

--- testi.js
+++ testi.js
@@ -1,1 +1,33 @@
-var = 5;
+/*var pelaaja = {
+  nimi: "joona",
+  vari: "punainen",
+  puoli: "vasen",
+  peliMerkki: 69,
+  katisyys: "vasen"
+};
+var koira = {
+  rotu: "tasmania",
+  vari: "kulta",
+  numero: 8,
+  koko: "valtava",
+  maku: "suklaa"
+};
+console.log(pelaaja.peliMerkki);
+console.log(koira.numero);
+*/
+//array
+/*var nimiLista = ["jaakko", "eero", "juho", "katri"];
+console.log(nimiLista);
+var nimi = nimiLista[2];
+console.log(nimi);
+*/
+//console.log(nimiLista[0]);
+//console.log(nimiLista.length);
+/*var randomLista = [1, 2, ["onppu", "pansku"], 5, {merkki: "samsung"}];
+var numero = randomLista[3];
+console.log(numero);
+var ruoka = randomLista[2][1];
+console.log(ruoka);
+console.log(randomLista[4].merkki);
+*/
+//listan metodit JS OSA7 8:00 youtube
