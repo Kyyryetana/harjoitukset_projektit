1f40b55b754e70deeacdae4084485dd9d30c1943
nikoalander1986 <niko.alander@hotmail.com>
2022-01-10 00:00:53 +0200

    JS opiskelua v1.12

--- nelilaskin/nelilaskin.js
+++ nelilaskin/nelilaskin.js
@@ -1,40 +1,21 @@
-//harjoitus1
-// let firstName = 'Niko';
-// let lastName = 'Alander';
-// let fullName = firstName + " " + lastName;
-// console.log(fullName);
-//harjoitus2
-// let name = 'Linda';
-// let greeting = 'Hi there';
-// let fullGreeting = greeting + "," + " " + name + "!";
-// console.log(fullGreeting);
-//harjoitus3
-// let myPoints = 3
-//
-// function add3Points() {
-//   myPoints += 3;
-// }
-//
-// function remove1Point() {
-//   myPoints -= 1;
-// }
-//
-// add3Points()
-// add3Points()
-// add3Points()
-// remove1Point()
-// remove1Point()
-// console.log(myPoints)
-//harjoitus4
-// console.log("2" + 2) // "22"
-// console.log(11 + 7) // 18
-// console.log(6 + "5") // "65"
-// console.log("My points: " + 5 + 9) // "My points: 59"
-// console.log(2 + 2) // 4
-// console.log("11" + "14") // "1114"
-//harjoitus5
-// let wrong = document.getElementById('error');
-// function buy() {
-//   wrong.innerText = "Something went wrong, please try again";
-// }
-//harjoitus6
+let num1 = 8;
+let num2 = 2;
+let sumEl = document.getElementById('sum-el');
+document.getElementById('num1-el').textContent = num1;
+document.getElementById('num2-el').textContent = num2;
+function add() {
+  let result = num1 + num2;
+  sumEl.textContent = "Sum: " + result;
+}
+function subtract() {
+  let result = num1 - num2;
+  sumEl.textContent = "Sum: " + result;
+}
+function divide() {
+  let result = num1 / num2;
+  sumEl.textContent = "Sum: " + result;
+}
+function multiply() {
+  let result = num1 * num2;
+  sumEl.textContent = "Sum: " + result;
+}
