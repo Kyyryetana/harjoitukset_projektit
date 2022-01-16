775664a3746902616ca7bf8290476e6d99ec65d2
nikoalander1986 <niko.alander@hotmail.com>
2022-01-16 02:49:03 +0200

    JS opiskelua v1.34

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,5 +1,6 @@
-let firstCard = 11;
+let firstCard = 3;
 let secondCard = 9;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
@@ -8,19 +9,28 @@ let sumEl = document.getElementById('sum-el');
 let cardsEl = document.getElementById('cards-el');
 
 function startGame() {
-if (sum <= 20) {
-  cardsEl.textContent = cardsEl.textContent + " " + firstCard + ", " + secondCard;
-  sumEl.textContent = sumEl.textContent + " " + sum;
-  messageEl.textContent = "Do you want new card?";
-  } else if (sum === 21) {
-    cardsEl.textContent = cardsEl.textContent + " " + firstCard + ", " + secondCard;
-    sumEl.textContent = sumEl.textContent + " " + sum;
-    messageEl.textContent = "You've got Blackjack!";
-    hasBlackJack = true;
-  } else {
-    cardsEl.textContent = cardsEl.textContent + " " + firstCard + ", " + secondCard;
-    sumEl.textContent = sumEl.textContent + " " + sum;
-    messageEl.textContent = "You're out of the game!";
-    isAlive = false;
-  }
+  renderGame();
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+    sumEl.textContent = "Sum: " + sum;
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "You've got Blackjack!";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game!";
+        isAlive = false;
+    }
+    messageEl.textContent = message;
+}
+
+function newCard() {
+  let thirdCard = 9;
+  sum += thirdCard;
+  cards.push(thirdCard);
+  console.log(cards);
+  renderGame()
 }
